e3c67c64c2d42819f28c3bc5f8982693df827daa
SAEBOMPAK <saebompak@gmail.com>
2021-03-18 13:27:55 +0900

    updated local storage

--- src/components/LocalStorage.js
+++ src/components/LocalStorage.js
@@ -35,41 +35,44 @@ const initialList = [
 
 
 const LocalStorage = () => {
-  const [list, setList] = useState(initialList);
+  const [lists, setLists] = useState(initialList);
 
-  const handleChange = (event) => {
-
-  }
-  
-  const handleAdd = (e) => {
-    e.preventDefault();
-
-
-  }
-  
   return (
     <>
     <div style={styles.content}>
       <h2 style={styles.title}>Todo lists</h2>
       <ul>
-        {list.map((item, index) => (
+        {lists.map((item, index) => (
           <li key={index} style={styles.list}>{item.text}</li>
         ))}
       </ul>
-      <ul>
 
-      </ul>
-      <form onSubmit={handleAdd}>
-        <input style={styles.input} type="text" placeholder="Item Name" onChange={handleChange} required />
-        <button style={styles.input} type="button">+ Add Item</button>
-      </form>
+      <InputText handleSubmit = {(list) => {
+        setLists(lists.concat(list))
+      }}
+      />
     </div>
     </>
     )
   }
   
+  const InputText = (props) => {
+    const [value, setValue] = useState('')
+    return (
+      <form onSubmit={(e) =>{
+        e.preventDefault();
+        props.handleSubmit(value);
+        console.log(value)
+        setValue('');
+      }}>
+        <input style={styles.input} type="text" value={value} onChange={e => setValue(e.target.value)}/>
+        <button style={styles.input} type="button">+ Add Item</button>
+      </form>
+    )
+  }
   export default LocalStorage
 
 //https://www.robinwieruch.de/react-add-item-to-list
 //https://medium.com/@dondeveloper/creating-a-shopping-list-app-using-react-hooks-bfd231cad813
-//이거보고 [value, setValue] 만들어서 concat하는 식으로 해볼것
+//https://www.digitalocean.com/community/tutorials/how-to-build-a-react-to-do-app-with-react-hooks
+//이거 따라해볼것,,
