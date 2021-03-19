db949fd4ff0767aba39bac70b458edaf67596890
SAEBOMPAK <saebompak@gmail.com>
2021-03-19 13:56:01 +0900

    updated add todo

--- src/components/LocalStorage.js
+++ src/components/LocalStorage.js
@@ -27,15 +27,21 @@ const initialList = [
     text: 'Drinking coffee',
     done: false,
   },
-  // {
-  //   id: 'b',
-  //   name: 'Robin',
-  // },
 ];
 
 
 const LocalStorage = () => {
   const [lists, setLists] = useState(initialList);
+  const addLists = text => {
+    const newLists = [...lists, { text }];
+    setLists(newLists);
+  };
+
+  const completeTodo = index => {
+    const newLists = [...lists];
+    newLists[index].isCompleted = true;
+    setLists(newLists);
+  };
 
   return (
     <>
@@ -46,29 +52,32 @@ const LocalStorage = () => {
           <li key={index} style={styles.list}>{item.text}</li>
         ))}
       </ul>
-
-      <InputText handleSubmit = {(list) => {
-        setLists(lists.concat(list))
-      }}
-      />
+      <InputText addLists={addLists} />
     </div>
     </>
     )
   }
   
-  const InputText = (props) => {
-    const [value, setValue] = useState('')
+  const InputText = ({ addLists }) => {
+    const [value, setValue] = useState("");
+    const handleSubmit = e => {
+      e.preventDefault();
+      if (!value) return;
+      addLists(value);
+      setValue("");
+    };
+  
     return (
-      <form onSubmit={(e) =>{
-        e.preventDefault();
-        props.handleSubmit(value);
-        console.log(value)
-        setValue('');
-      }}>
-        <input style={styles.input} type="text" value={value} onChange={e => setValue(e.target.value)}/>
-        <button style={styles.input} type="button">+ Add Item</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          style={styles.input}
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+        <button style={styles.input}>+ add list</button>
       </form>
-    )
+    );
   }
   export default LocalStorage
 
