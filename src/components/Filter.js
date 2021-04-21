ad18327fd1b176a7cb4bf0feee4012dc1f15b2d2
SAEBOMPAK <saebompak@gmail.com>
2021-04-22 08:50:29 +0900

    feat : add table in Filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -11,6 +11,13 @@ const styles = {
   h4 : {
     marginTop:'20px',
     marginBottom:'10px'
+  },
+  td : {
+    textAlign:'center',
+    padding:'10px',
+    borderRight:'1px solid #eee',
+    borderBottom:'1px solid #eee',
+    width:'50px'
   }
 }
 
@@ -39,14 +46,24 @@ const people = [
     name: 'George',
     age: 34,
     gender:'female'
-  }
+  },
 ];
 
 const Filter = () => {
-
+  const sum = people.map(_age => _age.age).reduce((a, b) => a + b)
 
   return (
     <div style={styles.margin}>
+      <table>
+        {people.map((person, index) => (
+          <tr>
+            <td style={styles.td}>{index+1}</td>
+            <td style={styles.td}>{person.name}</td>
+            <td style={styles.td}>{person.age}</td>
+            <td style={styles.td}>{person.gender}</td>
+          </tr>
+        ))} 
+      </table>
       <h4 style={styles.h4}>People aged under 60</h4>
       {people.filter(person => person.age < 60).map(filteredName => (
         <li style={styles.list}>{filteredName.name}, {filteredName.age}</li>
@@ -55,6 +72,8 @@ const Filter = () => {
       {people.filter(person => person.age < 60).filter(gen => gen.gender === 'male').map(filteredName => (
         <li style={styles.list}>{filteredName.name}, {filteredName.gender}</li>
       ))}
+      <h4 style={styles.h4}>Sum of ages</h4>
+      <li style={styles.list}>Total : {sum}</li>
     </div>
   )
 }
@@ -63,3 +82,4 @@ export default Filter
 
 
 // https://upmostly.com/tutorials/react-filter-filtering-arrays-in-react-with-examples
+// https://stackoverflow.com/questions/62358365/react-js-get-sum-of-numbers-in-array
