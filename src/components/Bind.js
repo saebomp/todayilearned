a6245f315a7fccaaa2e21f549ebbaf26abe895cd
SAEBOMPAK <saebompak@gmail.com>
2021-04-23 08:30:07 +0900

    fix : add arrow func in render

--- src/components/Bind.js
+++ src/components/Bind.js
@@ -10,7 +10,7 @@ const styles = {
     marginLeft:'5px'
   },
   marTop:{
-    marginTop:'50px'
+    marginTop:'20px'
   }
 }
 
@@ -18,19 +18,26 @@ export default class Bind extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title:'Click Me'
+      title:'Click Me',
+      title2:'Click Me'
     }
     this.handleClick = this.handleClick.bind(this);
   }
   handleClick() {
     this.setState({title:'Clicked'})
   }
+  handleClick2() {
+    this.setState({title2:'Clicked'})
+  }
   render() {
     return (
       <div style={styles.mar}>
         <button style={styles.btn} onClick={this.handleClick}>{this.state.title}</button>
         <p style={styles.marTop}>If you need to have access to the parent component in the handler, you also need to bind the function to the component instance</p>
-        <p></p>
+        <p style={styles.marTop}>
+          <button style={styles.btn} onClick={() => this.handleClick2()}>{this.state.title2}</button>
+        </p>
+        <p style={styles.marTop}>Arrow Function in Render : Using an arrow function in render creates a new function each time the component renders, which may break optimizations based on strict identity comparison.</p>
       </div>
     )
   }
