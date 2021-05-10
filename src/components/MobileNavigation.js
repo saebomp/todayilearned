0fa52a4797d09c29341920b0a1d41d72101da3d6
SAEBOMPAK <saebompak@gmail.com>
2021-05-11 03:55:51 +0900

    feat: add state in mobile navigation

--- src/components/MobileNavigation.js
+++ src/components/MobileNavigation.js
@@ -1,4 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
+import { GoThreeBars } from 'react-icons/go';
+import { GoX } from 'react-icons/go';
 
 const styles = {
   mar: {
@@ -8,10 +10,39 @@ const styles = {
 
 
 const MobileNavigation = () => {
+const [open, setOpen] = useState(false);
+
 
   return (
-    <div style={styles.mar}>
-      ddd
+    <div style={styles.mar} className="mn">
+      <button className="nav-btn open-btn" onClick={() => setOpen(true)}>
+        <i><GoThreeBars /></i>
+      </button>
+      <img src="https://logos-download.com/wp-content/uploads/2016/03/Netflix_logo.png" alt="Logo" className="logo" />
+      <p className="text">Mobile Navigation</p>
+      <div className={`${open ? "nav nav-black vision" : "nav nav-black"}`}> 
+        <div className={`${open ? "nav nav-red vision" : "nav nav-red"}`}> 
+          <div className={`${open ? "nav nav-white vision" : "nav nav-white"}`}> 
+            <button className="nav-btn close-btn" onClick={() => setOpen(false)}>
+              <i><GoX /></i>
+            </button>
+            <img src="https://logos-download.com/wp-content/uploads/2016/03/Netflix_logo.png" alt="Logo" className="logo" />
+            <ul className="list1">
+              <li><a href="#">Teams</a></li>
+              <li><a href="#">Locations</a></li>
+              <li><a href="#">Life at Netflix</a></li>
+              <li>
+                <ul>
+                  <li><a href="#">Netflix culture memo</a></li>
+                  <li><a href="#">Work life balance</a></li>
+                  <li><a href="#">Inclusion, diversity</a></li>
+                  <li><a href="#">Blog</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   )
   }
@@ -19,3 +50,14 @@ const MobileNavigation = () => {
 export default MobileNavigation
 
 //https://github.com/bradtraversy/50projects50days/blob/master/netflix-mobile-navigation/index.html
+
+// const open_btn = document.querySelector('.open-btn')
+// const close_btn = document.querySelector('.close-btn')
+// const nav = document.querySelectorAll('.nav')
+
+// open_btn.addEventListener('click', () => {
+//   nav.forEach(el => el.classList.add('visible'))
+// })
+// close_btn.addEventListener('click', ()=>{
+//   nav.forEach(el => el.classList.remove('visible'))
+// })
