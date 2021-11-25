4f9cfdd62b6c6a5a6799b8660ba10a2be91720ac
SAEBOMPAK <saebompak@gmail.com>
2021-11-25 15:37:27 -0800

    layout menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,28 +13,29 @@ const Menu = () => {
 
   return (
     <div className="sideMenu">
-        <header className="header">
-            <div>Logo</div>
-            <ul className="float">
-                <li>Menu1</li>
-                <li>Menu2</li>
-                <li>Menu3</li>
-                <li>Menu4</li>
-                <li>Menu5</li>
-                <li>Menu6</li>
-                <li>Menu7</li>
-                <li>Menu8</li>
-            </ul>
-        </header>
-        <div>
+        
+        <div className="contents">
             <div>contents</div>
             <div>contents</div>
             <div>contents</div>
             <div>contents</div>
         </div>
-        
+        <ul className="menu">
+          <li>Menu1</li>
+          <li>Menu2</li>
+          <li>Menu3</li>
+          <li>Menu4</li>
+          <li>Menu5</li>
+          <li>Menu6</li>
+          <li>Menu7</li>
+          <li>Menu8</li>
+        </ul>
     </div>
     )
   }
   
 export default Menu
+
+
+
+// https://dribbble.com/shots/16878702-Sidebar-Navigation
