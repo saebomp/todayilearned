f2b8154ccbd05f818ffcca65bc0e232a7b9ebab4
SAEBOMPAK <saebompak@gmail.com>
2021-11-25 16:26:46 -0800

    toggle class

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,27 +9,30 @@ const styles = {
 }
 
 const Menu = () => {
-
-
+  const [style, setStyle] = useState(true)
+  const handleSlide = () => {
+    console.log('ddddd')
+    setStyle(!style)
+  }
   return (
     <div className="sideMenu">
-        
-        <div className="contents">
-            <div>contents</div>
-            <div>contents</div>
-            <div>contents</div>
-            <div>contents</div>
-        </div>
-        <ul className="menu">
-          <li>Menu1</li>
-          <li>Menu2</li>
-          <li>Menu3</li>
-          <li>Menu4</li>
-          <li>Menu5</li>
-          <li>Menu6</li>
-          <li>Menu7</li>
-          <li>Menu8</li>
-        </ul>
+      <div className={style ? "contents" : "contents active"}>
+          <div>contents</div>
+          <div>contents</div>
+          <div>contents</div>
+          <div>contents</div>
+      </div>
+      <ul className={style ? "menu" : "menu active"}>
+        <li>Menu1</li>
+        <li>Menu2</li>
+        <li>Menu3</li>
+        <li>Menu4</li>
+        <li>Menu5</li>
+        <li>Menu6</li>
+        <li>Menu7</li>
+        <li>Menu8</li>
+        <div className="slide" onClick={handleSlide}>Click</div>
+      </ul>
     </div>
     )
   }
