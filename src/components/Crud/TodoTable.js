6a4a3def1915bc9fdf9af2801c1d9546cf9c47b4
SAEBOMPAK <saebompak@gmail.com>
2021-12-22 20:00:44 -0800

    styling

--- src/components/Crud/TodoTable.js
+++ src/components/Crud/TodoTable.js
@@ -13,10 +13,23 @@ const TodoTable = ({todo, handleDelete, handleInitialTodo, handleToggle}) => {
         <table className='table'>
             {todo.map((list,index) => (
             <tr>
-                <td><input type="checkbox" checked={list.active} onClick={() => handleToggle(index)} /></td>
-                <td>{list.item}</td>
+                <td>
+                    <input 
+                        type="checkbox" 
+                        checked={list.active} 
+                        onClick={() => handleToggle(index)} />
+                    </td>
+                <td>
+                    <span style={list.active? {textDecoration:'line-through'} : null}>
+                        {list.item}
+                    </span>
+                </td>
                 <td className='tdl'>
-                    <button className="btn" onClick={() => handleDelete(list.id)}>x</button>
+                    <button 
+                        className="btn" 
+                        onClick={() => handleDelete(list.id)}>
+                        x
+                    </button>
                     <button className="btn" onClick={()=> handleEdit(list)}>Edit</button>
                 </td>
             </tr>
