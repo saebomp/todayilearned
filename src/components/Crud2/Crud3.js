dca0df826a3b5d949b4647fb29b7761c92f9dd46
SAEBOMPAK <saebompak@gmail.com>
2022-01-28 18:09:14 -0800

    add handleUpdate

--- src/components/Crud2/Crud3.js
+++ src/components/Crud2/Crud3.js
@@ -14,7 +14,8 @@ class Crud3 extends Component {
                 id:2,
                 firstname:'Doe',
                 status:false}
-        ]
+        ],
+        editing:false,
       }
     }
 
@@ -38,19 +39,40 @@ handleDelete = (id) => {
         users:newUser
     })
 }
+handleEdit = (user) => {
+    this.setState({editing:true, initialUser: {...user}})
+}
+handleUpdate = (e) => {
+    e.preventDefault()
+    const updatedUser = this.state.users.map(user => (user.id == this.initialUser.id))
+    this.setState({
+        editing:false,
+        users:{}
+    })
+}
 render = () => {
     return (
         <>
-        <div>
+        {!this.state.editing ? (
+            <div>
             <input type="text" placeholder="first name" onChange={this.handleChange} />
             <button onClick={this.handleSubmit}>Submit</button>
-        </div>        
+        </div> 
+        ) : (
+        <div>
+            <input type="text" placeholder="first name" onChange={this.handleChange} />
+            <button onClick={() => this.setState({editing:false})}>Cancle</button>
+            <button onClick={this.handleUpdate}>Update</button>
+        </div> 
+        )
+        }    
         <div>
             {this.state.users.map(user => (
                 <div>
                     <span><input type="checkbox" checked={user.status} /></span>
                     {user.firstname}
                     <span><button onClick={() => this.handleDelete(user.id)}>x</button></span>
+                    <span><button onClick={() => this.handleEdit(user)}>edit</button></span>
                 </div>
             ))}
         </div>
